d6c6d9d325d629dfee93b8dcc16dbc83021d97a0
unknown <borisonekenobi@gmail.com>
2020-11-06 12:29:08 -0500

    Finished Code

--- main.py
+++ main.py
@@ -1,88 +1,113 @@
-def binToDec():
-  binary = input("Please enter a binary number: ")
-  decNum = 0
-  exp = 0
-  counter = 1
-  repeat = len(binary)
-  while counter <= repeat:
-      binD = binary[-1 * counter]
-      decNum = decNum + int(binD) * 2 ** exp
-      counter = counter + 1
-      exp = exp + 1
-  print(decNum)
-  return decNum
-
-
-def decToBin():
-  binNumber = []
-  remain = 0
-  decNum = int(input("Please enter a Decimal number: "))
-  while decNum != 0:
-    remain = decNum%2
-    decNum = int(decNum / 2)
-    binNumber.insert(0,remain)
-    if decNum == 1:
-      binNumber.insert(0,1)
-      break
-  print(*binNumber, sep='')
-  return binNumber
-
-def hexToDec():
-  decimal = []
+def binToDec(binary):
+    decNum = 0
+    exp = 0
     counter = 1
-    repeat = len(hex)
-    hex = input("Please enter a Hexidecimal number: ")
-    hex = hex.upper()
-    print(hex)
-    for i in hex:
-      if i == "1":
-        decimal.insert(-1,1)
-      elif i == "2":
-        decimal.insert(-1,2)
-      elif i == "3":
-        decimal.insert(-1,3)
-      elif i == "4":
-        decimal.insert(-1,4)
-      elif i == "5":
-        decimal.insert(-1,5)
-      elif i == "6":
-        decimal.insert(-1,6)
-      elif i == "7":
-        decimal.insert(-1,7)
-      elif i == "8":
-        decimal.insert(-1,8)
-      elif i == "9":
-        decimal.insert(-1,9)
-      elif i == "A":
-        decimal.insert(-1,10)
-      elif i == "B":
-        decimal.insert(-1,11)
-      elif i == "C":
-        decimal.insert(-1,12)
-      elif i == "D":
-        decimal.insert(-1,13)
-      elif i == "E":
-        decimal.insert(-1,14)
-      elif i == "F":
-        decimal.insert(-1,15)
-
-    for i in decimal:
-
-    return
+    repeat = len(binary)
+    while counter <= repeat:  # counter uses the length to make the Decimal number
+        binD = binary[-1 * counter]
+        decNum = decNum + int(binD) * 2 ** exp
+        counter = counter + 1
+        exp = exp + 1
+    return decNum
 
 
+def decToBin(decNum):
+    binNumber = []
+    remain = 0
+    while decNum != 0:  # checks if the Decimal number has reached 0
+        remain = decNum % 2  # checking for 0 or 1 remainder
+        decNum = int(decNum / 2)  # divides Decimal by 2
+        binNumber.insert(0, remain)
+        if decNum == 1:
+            binNumber.insert(0, 1)
+            break
+    return binNumber
 
 
+def hexToDec(hexNum):
+    hexNum = hexNum.upper()  # ensures all the letters are capitalized
+    exp = len(hexNum) - 1
+    sum = 0
+    for i in hexNum:  # converts the corresponding letters to numbers
+        if i == 'A':
+            i = 10
+        if i == 'B':
+            i = 11
+        if i == 'C':
+            i = 12
+        if i == 'D':
+            i = 13
+        if i == 'E':
+            i = 14
+        if i == 'F':
+            i = 15
+        sum = sum + int(i) * 16 ** exp  # does the exponent and multiplication
+        exp = exp - 1
+    return sum
 
 
+def decToHex(decNum):
+    hexNumber = []
+    remain = 0
+    while decNum != 0:  # checks if the Decimal number has reached 0
+        remain = decNum % 16
+        decNum = int(decNum / 16)  # divides Decimal by 16
+        hexNumber.insert(0, remain)
+        if decNum == 1:
+            hexNumber.insert(0, 1)
+            break
+        for i in range(0, len(hexNumber)):
+            if hexNumber[i] == 10:
+                hexNumber[i] = "A"
+            if hexNumber[i] == 11:
+                hexNumber[i] = "B"
+            if hexNumber[i] == 12:
+                hexNumber[i] = "C"
+            if hexNumber[i] == 13:
+                hexNumber[i] = "D"
+            if hexNumber[i] == 14:
+                hexNumber[i] = "E"
+            if hexNumber[i] == 15:
+                hexNumber[i] = "F"
 
-system = int(input("Please enter a number between 1 and 5: "))
+    return hexNumber
 
-if system == 1:
-  binToDec()
 
-elif system == 2:
-  decToBin()
+def hexToBin(hexNum):
+    decNum = hexToDec(hexNum)
+    binNum = decToBin(decNum)
+    return binNum
 
-elif system == 3:
-    hexToDec()
+
+print("Please enter the following options:", end='\n\n')
+print("Enter 1 for converting Binary to Decimal", end='\n\n')
+print("Enter 2 for converting Decimal to Binary", end='\n\n')
+print("Enter 3 for converting Hexadecimal to Decimal", end='\n\n')
+print("Enter 4 for converting Decimal to Hexadecimal", end='\n\n')
+print("Enter 5 for converting Hexadecimal to Binary", end='\n\n')
+print("Any other number to exit", end='\n\n')
+while True:
+    system = int(input("Please enter a number between 1 and 5: "))
+
+    if system == 1:
+        binary = input("Please enter a binary number: ")
+        print(binToDec(binary))
+
+    elif system == 2:
+        decNum = int(input("Please enter a Decimal number: "))
+        print(*decToBin(decNum), sep='')
+
+    elif system == 3:
+        hexNum = input("Please enter a Hexadecimal number: ")
+        print(hexToDec(hexNum))
+
+    elif system == 4:
+        decNum = int(input("Please enter a Decimal number: "))
+        print(*decToHex(decNum), sep='')
+
+    elif system == 5:
+        hexNum = input("Please enter a Hexadecimal number: ")
+        print(*hexToBin(hexNum), sep='')
+
+    else:
+        break
